77dfbc3d3058d3f3af2ca2bd0add4db2dfa45803
Thontana Siriwannaporn <77087531+thontana1995@users.noreply.github.com>
2022-02-26 10:19:42 +0700

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,16 @@
+import random 
+import string
+
+length = int(input("Enter the lenght of password: "))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+all = lower + upper + num + symbols
+
+temp = random.sample(all,length)
+password = "".join(temp)
+
+print(password)
